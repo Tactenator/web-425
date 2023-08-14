0e3253d42e7a6be9e666dca14b8feb89b33dc6ce
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-08-14 16:42:33 -0500

    adding enhanced profile app

---
+++ week-2/enhanced-profile-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isLoggedIn: boolean = true
+  assignment: string = "Assignment 2.3 - Data Binding"
+}
