1282e0c6844995b45a95f55283e0e04e275612bc
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-09-04 09:26:30 -0500

    fixing comments

--- week-4/reactive-composer-app/src/app/composer-interface.ts
+++ week-4/reactive-composer-app/src/app/composer-interface.ts
@@ -1,7 +1,7 @@
 /**
  * Name: Trevor McLaurine
  * Date: 8/22/2023
- * Assignment: Exercise 4.4 - Async Pipe
+ * Assignment: Exercise 4.3 - Handling Form Events with Observables
  * Description: Interface for Composers
 **/
 export interface IComposer {
--- week-4/reactive-composer-app/src/app/contact/contact.component.ts
+++ week-4/reactive-composer-app/src/app/contact/contact.component.ts
@@ -1,7 +1,7 @@
 /**
  * Name: Trevor McLaurine
  * Date: 8/22/2023
- * Assignment: Exercise 4.4 - Async Pipe
+ * Assignment: Assignment: Exercise 4.3 - Handling Form Events with Observables
  * Description: Contact Component.
 **/
 import { Component } from '@angular/core';
